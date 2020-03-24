bfe0d351a79cb8978b32978407c238b59b292744
TitoGrine <tito999@gmail.com>
2020-03-24 19:42:54 +0000

    Adjusted primite GUI.

--- interface.py
+++ interface.py
@@ -1,35 +1,43 @@
 import pygame
-import levels
+import zhed_board
 import math
-import constants
+import puzzle_reader
 
 pygame.init()
 
 pygame.display.set_caption("Zhed")
-window = pygame.display.set_mode((800, 800))
 
-level = levels.Levels().get_level(1)
-width = 100
-heigth = 100
+level = puzzle_reader.get_puzzle(99)
 
+board = zhed_board.ZhedBoard.build_from_file(level).board_state
+
+side = len(level)
+
+window = pygame.display.set_mode((side * 50, side * 50))
+
+width = 50
+heigth = 50
 
 run = True
 while run:
     pygame.time.delay(100)
 
-    for i in range(0, 64):
-        x_pos = (i % 8) * 100
-        y_pos = (math.floor(i / 8)) * 100
-        tile = level[i]
 
-        if   (tile == constants.GOAL):
-            pygame.draw.rect(window, (255, 102,   0), (x_pos, y_pos, width, heigth))
-        elif (tile == constants.EMPTY):
-            pygame.draw.rect(window, (192, 192, 192), (x_pos, y_pos, width, heigth))
-        elif (tile == constants.FILLED):
-            pygame.draw.rect(window, (255, 204,   0), (x_pos, y_pos, width, heigth))
-        else:
-            pygame.draw.rect(window, (255, 255, 255), (x_pos, y_pos, width, heigth))
+    for i in range(side):
+        y_pos = i * 50
+
+        for j in range(side):
+            x_pos = j * 50
+            tile = board[i][j]
+
+            if   (tile == zhed_board.BoardState.GOAL):
+                pygame.draw.rect(window, (255, 102,   0), (x_pos, y_pos, width, heigth))
+            elif (tile == zhed_board.BoardState.EMPTY):
+                pygame.draw.rect(window, (192, 192, 192), (x_pos, y_pos, width, heigth))
+            elif (tile == zhed_board.BoardState.FILLED):
+                pygame.draw.rect(window, (255, 204,   0), (x_pos, y_pos, width, heigth))
+            else:
+                pygame.draw.rect(window, (255, 255, 255), (x_pos, y_pos, width, heigth))
         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
