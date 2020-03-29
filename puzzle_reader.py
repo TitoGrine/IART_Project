d8f4251879c538ad38f3eb2d266e592856d3389b
TitoGrine <tito999@gmail.com>
2020-03-30 00:51:36 +0100

    Started player interface.

--- puzzle_reader.py
+++ puzzle_reader.py
@@ -1,10 +1,12 @@
 import zhed_board
 
+
 def get_puzzle(number):
     board = read_file(number)
     padded_board = padd_board(board)
 
     return padded_board
+
 
 def read_file(number):
     missing_zeros = 3 - len(str(number))
@@ -20,11 +22,12 @@ def read_file(number):
 
     return board
 
+
 def padd_board(board):
     max_length = max(len(board[0]) - 1, len(board))
     side_length = max_length + 2
-    x_padding = round((max_length - (len(board[0]) - 1))/2) + 1
-    y_padding = round((max_length - len(board))/2) + 1
+    x_padding = round((max_length - (len(board[0]) - 1)) / 2) + 1
+    y_padding = round((max_length - len(board)) / 2) + 1
     limit = len(board)
     padded_board = [] + [[zhed_board.BoardState.EMPTY] * side_length] * y_padding
 
@@ -37,4 +40,22 @@ def padd_board(board):
         padded_board.append(row)
 
     return padded_board
-    
+
+
+def padd_raw_board(board):
+    max_length = max(len(board[0]) - 1, len(board))
+    side_length = max_length + 2
+    x_padding = round((max_length - (len(board[0]) - 1)) / 2) + 1
+    y_padding = round((max_length - len(board)) / 2) + 1
+    limit = len(board)
+    padded_board = [] + [''.join(['.'] * side_length)] * y_padding
+
+    for y in range(side_length - y_padding):
+        if y < limit:
+            row = ''.join(['.'] * x_padding) + board[y] + ''.join(['.'] * x_padding)
+        else:
+            row = ''.join(['.'] * side_length)
+
+        padded_board.append(row)
+
+    return padded_board
