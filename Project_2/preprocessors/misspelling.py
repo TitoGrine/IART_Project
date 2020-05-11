357c2470a00a7187915a6885f7db8df28aa23b42
TitoGrine <tito999@gmail.com>
2020-05-11 10:25:54 +0100

    Added misspelling processor.

---
+++ Project_2/preprocessors/misspelling.py
@@ -0,0 +1,25 @@
+import pkg_resources
+from symspellpy import SymSpell, Verbosity
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class Misspelling(BaseEstimator, TransformerMixin):
+
+    def __init__(self, max_edit_distance=3, prefix_length=5, transfer_casing=True):
+        self.max_edit_distance = max_edit_distance
+        self.prefix_length = prefix_length
+        self.transfer_casing = transfer_casing
+
+    def fit(self, X, y=None):
+        return self
+
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def transform(self, tweet):
+        sym_spell = SymSpell(max_edit_distance=self.max_edit_distance, prefix_length=self.prefix_length)
+        dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
+        sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+
+        for token in tweet:
+            suggestions = sym_spell.lookup(text, Verbosity.CLOSEST, max_edit_distance=2, include_unknown=True)
+            token = suggestions[0].term
