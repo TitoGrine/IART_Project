15ac93e5fe029128081eedc8ec472cd5e7cb3dae
TitoGrine <tito999@gmail.com>
2020-05-11 12:14:15 +0100

    Added stemming processor.

---
+++ Project_2/preprocessors/stemming.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import EnglishStemmer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class Tokenizer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, algorithm):
+
+        if algorithm == "lancaster":
+            self.stemmer = LancasterStemmer()
+        elif algorithm == "porter":
+            self.stemmer = PorterStemmer()
+        elif algorithm == "snowball":
+            self.stemmer = EnglishStemmer()
+
+    def fit(self, X, y=None):
+        return self
+
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def transform(self, tweet):
+        for token in tweet:
+            token = self.stemmer.stem(token)
+
+        return token
